b956cf5155111fa84ff45d2f41192e000beb5714
prav <pravallikagudipati@gmail.com>
2025-09-01 18:55:56 +0530

    first commit

---
+++ client/src/app/listing/[id]/page.tsx
@@ -0,0 +1,14 @@
+import { Suspense } from "react";
+import ProductDetailsSkeleton from "./productSkeleton";
+import ProductDetailsContent from "./productDetails";
+
+async function ProductDetailsPage({ params }: { params: { id: string } }) {
+
+  return (
+    <Suspense fallback={<ProductDetailsSkeleton />}>
+      <ProductDetailsContent id={params.id} />
+    </Suspense>
+  );
+}
+
+export default ProductDetailsPage;
