5c9c36ea93a31247eeab6205e3b3c2967dca705d
pravallika2108 <81466991+pravallika2108@users.noreply.github.com>
2025-10-06 21:43:00 +0530

    Update api.ts

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:3001";
+export const API_BASE_URL = "https://mern-ecommerce-deploy-to-render-9.onrender.com";
 
 export const API_ROUTES = {
   AUTH: `${API_BASE_URL}/api/auth`,
