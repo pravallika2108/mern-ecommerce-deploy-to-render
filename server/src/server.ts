d7cc48120563a2ad7c6bda5da1636b4b12755a08
pravallika2108 <81466991+pravallika2108@users.noreply.github.com>
2025-10-07 19:30:20 +0530

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,14 +16,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 10000;
 
+app.options("*", cors({
+  origin: 'https://mern-ecommerce-deploy-to-render-11.onrender.com',
+  credentials: true,
+}));
 
-app.use(
-  cors({
-    origin: 'https://mern-ecommerce-deploy-to-render-11.onrender.com',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  })
-);
 
 app.use(express.json());
 app.use(cookieParser());
