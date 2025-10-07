7a3ed9a9cce7d4dcfb7d42c6c3fa922473c8aea2
pravallika2108 <81466991+pravallika2108@users.noreply.github.com>
2025-10-07 15:36:32 +0530

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -21,6 +21,7 @@ app.use(
   cors({
     origin: 'https://mern-ecommerce-deploy-to-render-11.onrender.com',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   })
 );
 
