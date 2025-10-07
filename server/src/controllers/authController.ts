44afce6952e8526d84adfc7ad6b3e401f756e111
pravallika2108 <81466991+pravallika2108@users.noreply.github.com>
2025-10-07 14:14:09 +0530

    Update authController.ts

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -26,13 +26,13 @@ async function setTokens(
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "None",
     maxAge: 60 * 60 * 1000,
   });
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "None",
     maxAge: 7 * 24 * 60 * 60*1000,
   });
 }
