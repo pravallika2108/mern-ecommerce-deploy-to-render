5535f67724379540a7af153cbad35b99fb063730
pravallika2108 <81466991+pravallika2108@users.noreply.github.com>
2025-10-07 18:42:50 +0530

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 10000;
 
 app.use(
   cors({
-    origin: 'https://mern-ecommerce-deploy-to-render-11.onrender.com',
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   })
