9929839c5e48b66fc759ed4fd9e86c35f1bda3c2
pravallika2108 <81466991+pravallika2108@users.noreply.github.com>
2025-10-08 21:15:12 +0530

    Update middleware.ts

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,77 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
-import { jwtVerify } from "jose";
 
 const publicRoutes = ["/auth/register", "/auth/login"];
-const superAdminRoutes = ["/super-admin", "/super-admim/:path*"];
-const userRoutes = ["/home"];
 
 export async function middleware(request: NextRequest) {
   const accessToken = request.cookies.get("accessToken")?.value;
   const { pathname } = request.nextUrl;
 
-  if (accessToken) {
-    try {
-      const { payload } = await jwtVerify(
-        accessToken,
-        new TextEncoder().encode(process.env.JWT_SECRET)
-      );
-      const { role } = payload as {
-        role: string;
-      };
-
-      if (publicRoutes.includes(pathname)) {
-        return NextResponse.redirect(
-          new URL(
-            role === "SUPER_ADMIN" ? "/super-admin" : "/home",
-            request.url
-          )
-        );
-      }
-
-      if (
-        role === "SUPER_ADMIN" &&
-        userRoutes.some((route) => pathname.startsWith(route))
-      ) {
-        return NextResponse.redirect(new URL("/super-admin", request.url));
-      }
-      if (
-        role !== "SUPER_ADMIN" &&
-        superAdminRoutes.some((route) => pathname.startsWith(route))
-      ) {
-        return NextResponse.redirect(new URL("/home", request.url));
-      }
-
-      return NextResponse.next();
-    } catch (e) {
-      console.error("Token verification failed", e);
-      const refreshResponse = await fetch(
-        "https://mern-ecommerce-deploy-to-render-11.onrender.com/api/auth/refresh-token",
-        {
-          method: "POST",
-          credentials: "include",
-        }
-      );
-
-      if (refreshResponse.ok) {
-        const response = NextResponse.next();
-        response.cookies.set(
-          "accessToken",
-          refreshResponse.headers.get("Set-Cookie") || ""
-        );
-        return response;
-      } else {
-        //ur refresh is also failed
-        const response = NextResponse.redirect(
-          new URL("/auth/login", request.url)
-        );
-        response.cookies.delete("accessToken");
-        response.cookies.delete("refreshToken");
-        return response;
-      }
-    }
+  // If user has access token and trying to access public routes, redirect to home
+  if (accessToken && publicRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL("/home", request.url));
   }
 
-  if (!publicRoutes.includes(pathname)) {
+  // If no access token and trying to access protected routes, redirect to login
+  if (!accessToken && !publicRoutes.includes(pathname)) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
 
